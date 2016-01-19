99e1912849ea3efd717821c9730f0fe448649145
nathandud <dudleynathan@byu.net>
2016-01-18 19:23:13 -0700

    Initial Commit

---
+++ Pickup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pickup
+//
+//  Created by Nathan Dudley on 1/18/16.
+//  Copyright © 2016 Pickup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
