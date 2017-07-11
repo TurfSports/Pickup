24f5840d6a38719fd5e8b18538964028376f63d6
Justin Carver <15Jcarver202@gmail.com>
2017-07-11 11:43:01 -0600

    added setup for player class

--- Pickup/Player.swift
+++ Pickup/Player.swift
@@ -7,14 +7,43 @@
 //
 
 import Foundation
+import UIKit
 
 class Player {
     
-    let id:String
-    let username:String
+    private let kUserName = "name"
+    private let kJoinedGames = "joinedGames"
+    private let kUserImage = "userJoined"
     
-    init (id:String, username: String) {
+    let id: String
+    var userName: String
+    var userImage: UIImage?
+    var userImageEndpoint: String
+    var joinedGames: [Game]
+    
+    init(id:String, username: String, userImage: UIImage?, joinedGames: [Game], userImageEndpoint: String) {
         self.id = id
-        self.username = username
+        self.userName = username
+        self.userImage = userImage
+        self.joinedGames = joinedGames
+        self.userImageEndpoint = userImageEndpoint
+    }
+    
+    init?(dictionary: [String: Any]) {
+        
+        guard let id = dictionary.first?.key,
+        let userName = dictionary[kUserName] as? String,
+        let joinedGames = dictionary[kJoinedGames] as? [Game],
+        let userImageEndpoint = dictionary[kUserImage] as? String
+            
+        else { return nil }
+        
+        self.id = id
+        self.userName = userName
+        self.joinedGames = joinedGames
+        self.userImageEndpoint = userImageEndpoint
+        ImageController.imageForURL(url: userImageEndpoint) { (image) in
+            self.userImage = image
+        }
     }
 }
