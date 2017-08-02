289642f5e31414eeef6e2a1e8b83282e7a6e5ad2
Justin Carver <15Jcarver202@gmail.com>
2017-08-02 17:00:58 -0600

    Updated podfile with facebook authentication

--- Podfile
+++ Podfile
@@ -6,6 +6,7 @@ target ‘Pickup’ do
   use_frameworks!
 
 pod 'FBSDKLoginKit'
+pod 'Firebase/Auth'
 pod ‘Firebase/Core’
 pod ‘Firebase/Database’
 pod ‘Firebase/Storage’
--- Pods/Target Support Files/Pods-Pickup/Pods-Pickup-frameworks.sh
+++ Pods/Target Support Files/Pods-Pickup/Pods-Pickup-frameworks.sh
@@ -91,6 +91,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/GoogleToolboxForMac/GoogleToolboxForMac.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Parse/Parse.framework"
   install_framework "$BUILT_PRODUCTS_DIR/ParseUI/ParseUI.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/nanopb/nanopb.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Bolts/Bolts.framework"
@@ -100,4 +101,5 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/GoogleToolboxForMac/GoogleToolboxForMac.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Parse/Parse.framework"
   install_framework "$BUILT_PRODUCTS_DIR/ParseUI/ParseUI.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/nanopb/nanopb.framework"
 fi
