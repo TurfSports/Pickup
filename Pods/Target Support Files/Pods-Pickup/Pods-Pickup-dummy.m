031c1a8e98a801fb29b0f976460118f9882bedfe
Justin Carver <15Jcarver202@gmail.com>
2017-11-09 14:54:21 -0700

    Updated names of app and file names

--- Pods/Target Support Files/Pods-Pickup/Pods-Pickup-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Pickup : NSObject
-@end
-@implementation PodsDummy_Pods_Pickup
-@end
---
+++ Pods/Target Support Files/Pods-Turf/Pods-Turf-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Turf : NSObject
+@end
+@implementation PodsDummy_Pods_Turf
+@end
